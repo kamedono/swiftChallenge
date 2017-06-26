1c66ffafc9ff3a945c21652761d3edf0c2eea1b8
HigakiToshiki <higakitoshiki@team-lab.com>
2017-06-26 23:26:16 +0900

    Initial Commit

---
+++ SwiftChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftChallenge
+//
+//  Created by 檜垣 俊希 on 2017/06/26.
+//  Copyright © 2017年 檜垣 俊希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftChallengeTests/SwiftChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftChallengeTests.swift
+//  SwiftChallengeTests
+//
+//  Created by 檜垣 俊希 on 2017/06/26.
+//  Copyright © 2017年 檜垣 俊希. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftChallenge
+
+class SwiftChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
